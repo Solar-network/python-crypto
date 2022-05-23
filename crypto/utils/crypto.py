37ed4b0c3e2dda4be757074f1b7a209bf81d3e76
c0nsol3 <consol3@protonmail.com>
2022-05-23 09:39:41 +0200

    update docstrings and args in crypto.py

--- crypto/utils/crypto.py
+++ crypto/utils/crypto.py
@@ -6,21 +6,61 @@ from crypto.schnorr import schnorr
 from crypto.identity.private_key import PrivateKey
 
 
-def sign_schnorr(msg: str, private_key: PrivateKey, nonce: int = None) -> str:
+def sign_schnorr(msg: bytes, private_key: PrivateKey, nonce: int = None) -> str:
+    """Signs a message using Schnorr BIP340
+
+    Args:
+        msg (bytes): a message you wish to sign
+        private_key (PrivateKey): private key object
+        nonce (int): deterministic nonce
+
+    Returns:
+        str: returnes a hex string of a signature
+    """
     signature = ssa.sign(msg, private_key.to_hex(), nonce)
     return signature.serialize().hex()
 
 
-def sign_schnorr_legacy(msg: str, private_key: PrivateKey) -> str:
+def sign_schnorr_legacy(msg: bytes, private_key: PrivateKey) -> str:
+    """Signs a message using Legacy Schnorr
+
+    Args:
+        msg (bytes): a message you wish to sign
+        private_key (PrivateKey): private key object
+        nonce (int): deterministic nonce
+
+    Returns:
+        str: returnes a hex string of a signature
+    """
     msg = hashlib.sha256(msg).digest()
     secret = unhexlify(private_key.to_hex())
     signature = schnorr.bcrypto410_sign(msg, secret).hex()
     return signature
 
 
-def verify_schnorr(msg: str, sender_public_key: str, signature: str) -> bool:
-    return ssa.verify(msg, sender_public_key, signature)
+def verify_schnorr(msg: bytes, public_key: str, signature: str) -> bool:
+    """Verifies a message using Schnorr BIP340
+
+    Args:
+        msg (bytes): a message you wish to sign
+        public_key (str)
+        signature (str)
+
+    Returns:
+        bool
+    """
+    return ssa.verify(msg, public_key, signature)
 
 
-def verify_schnorr_legacy(msg: str, sender_public_key: str, signature: str) -> bool:
-    return schnorr.b410_schnorr_verify(msg, sender_public_key, signature)
+def verify_schnorr_legacy(msg: bytes, public_key: str, signature: str) -> bool:
+    """Verifies a message using Legacy Schnorr
+
+    Args:
+        msg (bytes): a message you wish to sign
+        public_key (str)
+        signature (str)
+
+    Returns:
+        bool
+    """
+    return schnorr.b410_schnorr_verify(msg, public_key, signature)
