fd76a66361c47d42df64793c2e8e9c56413b229a
c0nsol3 <consol3@protonmail.com>
2022-05-27 15:51:52 +0200

    refactor: make vote attribute an optional kwarg in voter builder and add set_votes method

--- crypto/transactions/builder/vote.py
+++ crypto/transactions/builder/vote.py
@@ -1,3 +1,5 @@
+import typing
+
 from crypto.constants import TRANSACTION_VOTE
 from crypto.identity.address import address_from_passphrase
 from crypto.transactions.builder.base import BaseTransactionBuilder
@@ -7,7 +9,7 @@ class Vote(BaseTransactionBuilder):
 
     transaction_type = TRANSACTION_VOTE
 
-    def __init__(self, vote, fee=None):
+    def __init__(self, vote=None, fee=None):
         """Create a second signature registration transaction
 
         Args:
@@ -16,10 +18,20 @@ class Vote(BaseTransactionBuilder):
         """
         super().__init__()
 
-        self.transaction.asset["votes"] = [vote]
+        self.transaction.asset["votes"] = []
+        if vote:
+            self.transaction.asset["votes"].append(vote)
 
         if fee:
             self.transaction.fee = fee
+
+    def set_votes(self, votes: typing.List[str]):
+        """Set votes/unvotes
+
+        Args:
+            votes (List[str]): list of votes
+        """
+        self.transaction.asset["votes"] = votes
 
     def sign(self, passphrase):
         self.transaction.recipientId = address_from_passphrase(passphrase)
