fead2fd5b58756522303d90da1ba98af30ee53e6
c0nsol3 <consol3@protonmail.com>
2022-05-27 15:58:03 +0200

    feat: support for v3 vote transaction

--- crypto/transactions/deserializers/vote.py
+++ crypto/transactions/deserializers/vote.py
@@ -1,7 +1,9 @@
 from binascii import hexlify
+from io import BytesIO
 
 from binary.unsigned_integer.reader import read_bit8
 
+from crypto.exceptions import SolarDeserializerException
 from crypto.transactions.deserializers.base import BaseDeserializer
 
 
@@ -9,23 +11,39 @@ class VoteDeserializer(BaseDeserializer):
     def deserialize(self):
         starting_position = int(self.asset_offset / 2)
 
-        vote_length = read_bit8(self.serialized, starting_position) & 0xFF
+        buffer = BytesIO(self.serialized[starting_position::])
+        vote_length = read_bit8(buffer.read(1))
 
         self.transaction.asset["votes"] = []
+        vote_offset = 0
 
-        for index in range(vote_length):
-            starting_position = self.asset_offset + 2 + (index * 2 * 34)
-            vote = hexlify(self.serialized)[starting_position : starting_position + 2 * 34].decode()
-            if vote[1] == "1":
-                operator = "+"
+        for _ in range(vote_length):
+            if self.transaction.version == 2 and buffer.read(1) != b"\xff":
+                buffer.seek(1)
+                vote_offset += 34
+                vote_buffer = buffer.read(34)
+                prefix = "+" if vote_buffer[0] == 1 else "-"
+                vote = f"{prefix}{vote_buffer[1::].hex()}"
             else:
-                operator = "-"
-            vote = "{}{}".format(operator, vote[2:])
+                if self.transaction.version == 2:
+                    length = read_bit8(buffer.read(1))
+                    vote_buffer = buffer.read(length)
+                    vote_offset += length + 2
+                else:
+                    length = read_bit8(buffer.read(1))
+                    vote_buffer = buffer.read(length)
+                    vote_offset += length + 1
+
+                prefix = "+" if vote_buffer[0] == 1 else "-"
+                vote = f"{prefix}{vote_buffer[1::].decode()}"
+
+            if len(vote) <= 1:
+                raise SolarDeserializerException("Invalid transaction data")
 
             self.transaction.asset["votes"].append(vote)
 
         self.transaction.parse_signatures(
-            hexlify(self.serialized).decode(), self.asset_offset + 2 + (vote_length * 34 * 2)
+            hexlify(self.serialized).decode(), self.asset_offset + 2 + (vote_offset * 2)
         )
 
         return self.transaction
--- crypto/transactions/serializers/vote.py
+++ crypto/transactions/serializers/vote.py
@@ -1,4 +1,4 @@
-from binascii import unhexlify
+from binascii import hexlify, unhexlify
 
 from binary.unsigned_integer.writer import write_bit8
 
@@ -12,10 +12,20 @@ class VoteSerializer(BaseSerializer):
         vote_bytes = []
 
         for vote in self.transaction["asset"]["votes"]:
-            if vote.startswith("+"):
-                vote_bytes.append("01{}".format(vote[1::]))
-            else:
-                vote_bytes.append("00{}".format(vote[1::]))
+            prefix = "01" if vote.startswith("+") else "00"
+            sliced = vote[1::]
+
+            if len(sliced) == 66:
+                vote_bytes.append(f"{prefix}{sliced}")
+                continue
+
+            # vote.length.toString(16).padStart(2, "0") + prefix + Buffer.from(sliced).toString("hex");
+            start = format(len(vote), "x").zfill(2)
+            vote_hex = f"{start}{prefix}{hexlify(sliced.encode()).decode()}"
+            if self.transaction["version"] == 2:
+                vote_hex = f"ff{vote_hex}"
+
+            vote_bytes.append(vote_hex)
 
         self.bytes_data += write_bit8(len(self.transaction["asset"]["votes"]))
         self.bytes_data += unhexlify("".join(vote_bytes))
