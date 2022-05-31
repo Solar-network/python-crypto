31308cbe8e2deab60c44262864921e24b1fc1d38
c0nsol3 <consol3@protonmail.com>
2022-05-31 17:54:51 +0200

    chore: preparation before building a package

--- Makefile
+++ Makefile
@@ -11,3 +11,12 @@ black:
 
 flake8:
 	flake8 .
+
+cleanup-dist:
+	rm -rf dist
+
+build-package:
+	python -m build
+
+upload-package:
+	python -m twine upload dist/*
--- setup.py
+++ setup.py
@@ -2,17 +2,20 @@ import sys
 
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 requires = ["base58", "binary-helpers", "coincurve", "btclib"]
 
 tests_require = [
-    "flake8>=4.0.1",
-    "flake8-import-order>=0.18.1",
-    "flake8-print>=5.0.0",
-    "flake8-quotes>=3.3.1",
-    "pytest>=3.6.1",
-    "isort>=5.10.1",
-    "pytest-cov>=2.5.1",
-    "black>=22.3.0",
+    "flake8",
+    "flake8-import-order",
+    "flake8-print",
+    "flake8-quotes",
+    "pytest",
+    "isort",
+    "pytest-cov",
+    "black",
 ]
 
 extras_require = {"test": tests_require, "dev": requires + tests_require}
@@ -20,15 +23,18 @@ extras_require = {"test": tests_require, "dev": requires + tests_require}
 setup_requires = ["pytest-runner"] if {"pytest", "test", "ptr"}.intersection(sys.argv) else []
 
 setuptools.setup(
-    name="solarnetwork-crypto",
+    name="solar-crypto",
     description="A simple Cryptography Implementation in Python for the Solar Blockchain.",
     version="3.0.0",
     author="Solar-Network",
     author_email="hello@solar.org",
     url="https://github.com/Solar-network/python-crypto",
     packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=requires,
     extras_require=extras_require,
     tests_require=tests_require,
     setup_requires=setup_requires,
+    python_requires=">=3.6",
 )
