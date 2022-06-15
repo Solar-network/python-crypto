55414ce0526df9f70070d521efc7fecd4eee42a3
c0nsol3 <consol3@protonmail.com>
2022-06-15 22:35:04 +0200

    test: custom sort func

--- solar_crypto/transactions/builder/vote.py
+++ solar_crypto/transactions/builder/vote.py
@@ -1,4 +1,6 @@
+import re
 import typing
+from functools import cmp_to_key
 from math import trunc
 
 from solar_crypto.constants import SOLAR_TRANSACTION_VOTE, TRANSACTION_TYPE_GROUP
@@ -55,7 +57,40 @@ class Vote(BaseTransactionBuilder):
         self.transaction.asset["votes"] = votes
 
 
+def cmp(a: typing.List[typing.Union[int, str]], b: typing.List[typing.Union[int, str]]):
+    """
+    Compare two alphanum keys
+    """
+    return (a > b) - (a < b)
+
+
+def nat_cmp(a: str, b: str):
+    """
+    Natural comparison
+    """
+    convert = lambda text: int(text) if text.isdigit() else text.lower()  # noqa: E731
+    alphanum_key = lambda key: [convert(c) for c in re.split("([0-9]+)", key)]  # noqa: E731
+    return cmp(alphanum_key(a), alphanum_key(b))
+
+
+def sorter(
+    a: typing.Tuple[str, typing.Union[float, int]], b: typing.Tuple[str, typing.Union[float, int]]
+):
+    """
+    Sort using desc weight and asc by name
+    """
+    if b[1] > a[1]:
+        return 1
+    elif b[1] < a[1]:
+        return -1
+    else:
+        return nat_cmp(a[0], b[0])
+
+
 def sort_votes(votes: typing.Dict[str, typing.Union[float, int]]):
-    sorted_votes = sorted(votes.items(), key=lambda vote: vote[0], reverse=True)
-    sorted_votes = sorted(votes.items(), key=lambda vote: vote[1], reverse=True)
+    """
+    Sort votes using custom sorter function
+    """
+    sorter_fn = cmp_to_key(sorter)
+    sorted_votes = sorted(votes.items(), key=sorter_fn)
     return dict(sorted_votes)
