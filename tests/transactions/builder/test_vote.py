3d7cc3337941542cfa5c99811a48242a6dc5e884
c0nsol3 <consol3@protonmail.com>
2022-06-26 19:36:57 +0200

    test: vote order, tx input checks

--- tests/transactions/builder/test_vote.py
+++ tests/transactions/builder/test_vote.py
@@ -1,30 +1,23 @@
 from collections import OrderedDict
 
+import pytest
+
 from solar_crypto.configuration.network import set_network
 from solar_crypto.constants import SOLAR_TRANSACTION_VOTE, TRANSACTION_TYPE_GROUP
+from solar_crypto.exceptions import SolarInvalidTransaction
 from solar_crypto.networks.testnet import Testnet
 from solar_crypto.transactions.builder.vote import Vote, sort_votes
 
 set_network(Testnet)
 
 
-def test_vote_transaction():
+def test_vote_transaction_input_error():
     vote = {"deadlock": 50.3333}
 
     transaction = Vote()
-    transaction.set_votes(vote)
-    transaction.set_nonce(1)
-    transaction.sign("testing")
-    transaction_dict = transaction.to_dict()
-
-    assert transaction_dict["nonce"] == 1
-    assert transaction_dict["signature"]
-    assert transaction_dict["asset"]["votes"] == vote
-    assert transaction_dict["type"] is SOLAR_TRANSACTION_VOTE
-    assert transaction_dict["typeGroup"] == TRANSACTION_TYPE_GROUP.SOLAR.value
-    assert transaction_dict["fee"] == 100000000
-
-    transaction.verify()  # if no exception is raised, it means the transaction is valid
+    with pytest.raises(SolarInvalidTransaction) as e:
+        transaction.set_votes(vote)
+    assert str(e.value) == "Only two decimal places are allowed."
 
 
 def test_vote_transaction_multiple_votes():
@@ -41,12 +34,12 @@ def test_vote_transaction_multiple_votes():
     assert transaction_dict["asset"]["votes"] == vote
     assert transaction_dict["type"] is SOLAR_TRANSACTION_VOTE
     assert transaction_dict["typeGroup"] == TRANSACTION_TYPE_GROUP.SOLAR.value
-    assert transaction_dict["fee"] == 100000000
+    assert transaction_dict["fee"] == 9000000
 
     transaction.verify()  # if no exception is raised, it means the transaction is valid
 
 
-def test_vote_transaction_using_list():
+def test_vote_transaction_using_empty_list():
     transaction = Vote()
     transaction.set_votes(["-awesome"])
     transaction.set_nonce(1)
@@ -58,9 +51,38 @@ def test_vote_transaction_using_list():
     assert transaction_dict["asset"]["votes"] == {}
     assert transaction_dict["type"] is SOLAR_TRANSACTION_VOTE
     assert transaction_dict["typeGroup"] == TRANSACTION_TYPE_GROUP.SOLAR.value
-    assert transaction_dict["fee"] == 100000000
+    assert transaction_dict["fee"] == 9000000
 
     transaction.verify()  # if no exception is raised, it means the transaction is valid
+
+
+def test_vote_transaction_using_list_ok():
+    transaction = Vote()
+    transaction.set_votes(["awesome", "possom", "flossom"])
+    transaction.set_nonce(1)
+    transaction.sign("testing")
+    transaction_dict = transaction.to_dict()
+
+    assert transaction_dict["nonce"] == 1
+    assert transaction_dict["signature"]
+    assert OrderedDict(transaction_dict["asset"]["votes"]) == OrderedDict({
+        "awesome": 33.34,
+        "flossom": 33.33,
+        "possom": 33.33,
+    })
+    assert transaction_dict["type"] is SOLAR_TRANSACTION_VOTE
+    assert transaction_dict["typeGroup"] == TRANSACTION_TYPE_GROUP.SOLAR.value
+    assert transaction_dict["fee"] == 9000000
+
+    transaction.verify()  # if no exception is raised, it means the transaction is valid
+
+
+def test_vote_transaction_using_list_more_than_53():
+    transaction = Vote()
+
+    with pytest.raises(SolarInvalidTransaction) as e:
+        transaction.set_votes([str(x) for x in range(55)])
+    assert str(e.value) == "Unable to vote for more than 53 delegates"
 
 
 def test_sort_votes():
--- tests/transactions/deserializers/test_vote.py
+++ tests/transactions/deserializers/test_vote.py
@@ -2,7 +2,7 @@ from solar_crypto.transactions.deserializer import Deserializer
 
 
 def test_vote_deserializer():
-    serialized = "ff031e0200000002000100000000000000037fde73baaa48eb75c013fe9ff52a74a096d48b9978351bdcb5b72331ca37487c00e1f50500000000000108646561646c6f636ba9138ee49974901f9818d56459d1817c41fc2d51204abc07cea05b72cae00cffaf314057f06b009ce6a2f99c8774af171f209fac1a9ce15abb429648ea22321765cb"  # noqa
+    serialized = "ff031e0200000002000100000000000000037fde73baaa48eb75c013fe9ff52a74a096d48b9978351bdcb5b72331ca37487c4054890000000000000208646561646c6f636b46190366756eca0db1b484cb3b781b0af8f82f8639c52f25f637100d8adadcdde380c07924ada1cc1c1f5b6107cce1ff86b87ab66806ca1a8ae716e3555198cc7fa324a70f697b54"  # noqa
 
     deserializer = Deserializer(serialized)
     actual = deserializer.deserialize()
@@ -11,29 +11,16 @@ def test_vote_deserializer():
     assert actual.typeGroup == 2
     assert actual.type == 2
     assert actual.amount == 0
-    assert actual.fee == 100000000
+    assert actual.fee == 9000000
     assert actual.nonce == 1
-    assert actual.asset["votes"] == {"deadlock": 50.33}
+    assert actual.asset["votes"] == {"deadlock": 64.7, "fun": 35.3}
     assert (
         actual.senderPublicKey
         == "037fde73baaa48eb75c013fe9ff52a74a096d48b9978351bdcb5b72331ca37487c"
     )  # noqa
     assert (
         actual.signature
-        == "8ee49974901f9818d56459d1817c41fc2d51204abc07cea05b72cae00cffaf314057f06b009ce6a2f99c8774af171f209fac1a9ce15abb429648ea22321765cb"
+        == "b1b484cb3b781b0af8f82f8639c52f25f637100d8adadcdde380c07924ada1cc1c1f5b6107cce1ff86b87ab66806ca1a8ae716e3555198cc7fa324a70f697b54"
     )
 
     actual.verify()
-
-
-def test_vote_deserializer_multiple_votes():
-    serialized = "ff031e0200000002000100000000000000037fde73baaa48eb75c013fe9ff52a74a096d48b9978351bdcb5b72331ca37487c00e1f5050000000000020366756eca0d08646561646c6f636b46196300f3ff5eff1a2e083ab31762089ad95bebb3de8ee451442a5fd945c2d7773cfe5dfc1528d0c03533677b88cd9e4421395716f78e9f7519343d2fb43ef1ca8d"
-
-    deserializer = Deserializer(serialized)
-    actual = deserializer.deserialize()
-
-    assert actual.asset["votes"] == {"fun": 35.3, "deadlock": 64.7}
-    assert (
-        actual.senderPublicKey
-        == "037fde73baaa48eb75c013fe9ff52a74a096d48b9978351bdcb5b72331ca37487c"
-    )  # noqa
